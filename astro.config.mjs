aab1759594d1b03742f0db21c1f3736ad4d264e2
Shengtong Jin <shengtongjin@192.168.1.118>
2025-11-12 23:43:35 +0800

    chore: testing for github page

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://sjin66.github.io', // 你的 GitHub Pages URL
+  	base: '/my-astro-blog', // 如果你的仓库名不是 username.github.io，需要设置 base
 	integrations: [mdx(), sitemap()],
 });
