3b2ea3c3df97f40f6e25f0f4d6aec90ab0c43c2c
Shengtong Jin <shengtongjin@192.168.1.118>
2025-11-13 00:09:19 +0800

    fix: hyper link issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://sjin66.github.io', // GitHub Pages URL
-	integrations: [mdx(), sitemap()],
+  site: "https://sjin66.github.io",
+  base: "/my-astro-blog",
+  integrations: [mdx(), sitemap()],
 });
