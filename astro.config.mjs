239c03f918bdfe94ea6356b3e8cc3f629890faff
Shengtong Jin <shengtongjin@192.168.1.118>
2025-11-13 21:44:40 +0800

    chore: :hammer: add project configuration files and Git workflow
    Add Commitlint configuration for commit message validation
    
    Add EditorConfig configuration for consistent editor settings
    
    Add Husky hooks for pre-commit checks
    
    d dlint-staged configuration for staged files chegckin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 // @ts-check
 
-import mdx from "@astrojs/mdx";
-import sitemap from "@astrojs/sitemap";
-import { defineConfig } from "astro/config";
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://sjin66.github.io",
-  base: "/my-astro-blog",
-  integrations: [mdx(), sitemap()],
+  site: 'https://sjin66.github.io',
+  base: '/my-astro-blog',
+  integrations: [mdx(), sitemap(), react()],
 });
