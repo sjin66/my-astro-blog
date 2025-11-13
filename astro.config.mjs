5978010daf0fc026b3eefeb64cfbe0f41e13b830
Shengtong Jin <shengtongjin@192.168.1.118>
2025-11-13 23:43:46 +0800

    chore: :hammer: tailwind and shadcn set up

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,15 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://sjin66.github.io',
   base: '/my-astro-blog',
   integrations: [mdx(), sitemap(), react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
